786485c5dfba4aa11d82a2ed3434e10ff39cba39
Ali Emre Şafak <aliemresafak@gmail.com>
2022-10-12 19:05:37 +0300

    add movie entity

--- src/movie/entities/movie.entity.ts
+++ src/movie/entities/movie.entity.ts
@@ -1,1 +1,31 @@
-export class Movie {}
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Movie {
+  @PrimaryColumn()
+  id:number
+  @Column()
+  adult: boolean;
+  @Column()
+  backdrop_path: string;
+  @Column()
+  original_language: string;
+  @Column()
+  original_title: string;
+  @Column()
+  overview: string;
+  @Column()
+  popularity: number;
+  @Column()
+  poster_path: string;
+  @Column()
+  release_date: string;
+  @Column()
+  title: string;
+  @Column()
+  video: boolean;
+  @Column()
+  vote_average: number;
+  @Column()
+  vote_count: number;
+}
