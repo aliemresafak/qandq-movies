094d584025ae80537fd6513b5530465bb72ac93a
Ali Emre Şafak <aliemresafak@gmail.com>
2022-10-13 23:41:30 +0300

    add movie to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-import {MovieModule} from "./movie/movie.module"
+import { MovieModule } from './movie/movie.module';
 
 @Module({
   imports: [DatabaseModule, MovieModule, ConfigModule.forRoot()],
