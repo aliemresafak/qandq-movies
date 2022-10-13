c450fbf1ea64e54253702286a605d63cbe9d5fd0
Ali Emre Şafak <aliemresafak@gmail.com>
2022-10-13 23:41:44 +0300

    remove unused controller

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,20 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { MovieService } from './movie.service';
 
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
-
-  @Get("?")
+  @Get('?')
   findAll(@Query() query) {
-    const { amount } = query
+    const { amount } = query;
     return this.movieService.findAll(amount);
   }
 
   @Get(':id')
   findOne(@Param('id') id: number) {
-    console.log(typeof id, id)
+    console.log(typeof id, id);
     return this.movieService.findOne(Number(id));
   }
 }
