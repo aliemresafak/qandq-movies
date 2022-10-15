80c7cd5fecf996e2f869b0e96ff41f0926ef732b
Ali Emre Şafak <aliemresafak@gmail.com>
2022-10-15 18:32:44 +0300

    add validation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {cors: true});
-    const config = new DocumentBuilder()
-        .setTitle("Movies API")
-        .setDescription("The movies API for Q&Q")
-        .build()
-    const document = SwaggerModule.createDocument(app, config)
-    SwaggerModule.setup("", app, document)
-    await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('Movies API')
+    .setDescription('The movies API for Q&Q')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
 }
 
 bootstrap();
